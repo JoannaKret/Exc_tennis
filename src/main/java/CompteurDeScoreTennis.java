8b059d0808dc1809f15b4810fda870143c295dc9
teban86 <nauleaustephane@gmail.com>
2022-07-27 13:49:49 +0200

    test debut

--- src/main/java/CompteurDeScoreTennis.java
+++ src/main/java/CompteurDeScoreTennis.java
@@ -1,2 +1,12 @@
+
+
 public class CompteurDeScoreTennis {
+	
+	public static void main(String[] args) {
+		hello();
+	}
+	
+	public static void hello() {
+		System.out.println( "Hello World");
+	}
 }
